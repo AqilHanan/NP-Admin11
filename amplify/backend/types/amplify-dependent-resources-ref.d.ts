6de39ab229740e69084b9edd8c01a19a85490d5a
AqilHanan <148228299+AqilHanan@users.noreply.github.com>
2025-02-11 17:52:49 +0800

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "MooRide20Data": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
